e3bfead6c37b85da8d4cdd38b47a4bc9063c76dc
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-30 12:09:23 -0300

    changing syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-import { Footer } from "./components/Footer";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
-  );
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
